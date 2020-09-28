7be95d6b8a5e3cbc664239ceeb892520237c1e92
Alumno <ulsa@d105.local>
2020-09-28 14:08:55 -0700

    Initial Commit

---
+++ prueba/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  prueba
+//
+//  Created by Alumno on 28/09/20.
+//  Copyright © 2020 Alumno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
